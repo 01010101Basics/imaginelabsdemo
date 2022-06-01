23cdd06cf38a0ab571d656e1dfbe56f71a25a638
Ken Goddard <closerwalk@gmail.com>
2022-06-01 07:38:36 -0700

    update index.php

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -14,6 +14,22 @@ $this->pageTitle=Yii::app()->name;
 <p><strong>For practical uses cases such as:</strong></br></br> Testing ELB/Scalling in AWS with deployment templates</br>
 </p>
 <?php //echo CHtml::image(Yii::app()->baseUrl . '/img/netwrk.jpg', "this is alt tag of image", array('height'=>'450', 'width'=>'900')); ?>
+<?php
+  $curl_handle=curl_init();
+  curl_setopt($curl_handle,CURLOPT_URL,'http://169.254.169.254/latest/meta-data/placement/availability-zone');
+  curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2);
+  curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,1);
+  $buffer = curl_exec($curl_handle);
+  curl_close($curl_handle);
+  if (empty($buffer)){
+      
+  }
+  else{
+      $region= $buffer;
+  }
+  echo "AWS AZ: <b>".$region."</b>";
+?>
+
 <!DOCTYPE html>
 <html>
 <body>
