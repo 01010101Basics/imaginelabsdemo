534160c0d7de1f11b2be41ebb597aeb855ff5a80
Ken Goddard <closerwalk@gmail.com>
2022-06-07 18:51:42 -0700

    sql change

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 01, 2022 at 07:04 PM
+-- Generation Time: Jun 07, 2022 at 04:41 PM
 -- Server version: 10.5.15-MariaDB-0+deb11u1
 -- PHP Version: 7.4.28
 
@@ -20,8 +20,9 @@ SET time_zone = "+00:00";
 --
 -- Database: `users`
 --
-CREATE DATABASE users;
-USE DATABASE users;
+CREATE DATABASE IF NOT EXISTS `users` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `users`;
+
 -- --------------------------------------------------------
 
 --
@@ -42,7 +43,7 @@ CREATE TABLE `tbl_user` (
 INSERT INTO `tbl_user` (`id`, `username`, `password`, `email`) VALUES
 (2, 'kellieg', 'b9a15b9c79dd3a9f32fca0a8b3b51a6833f718698f6476686dc76709e605560d8f596ee87476b7e4fc70e38bba1324ccb975c96491f3ba621be35025b44be9f5', 'birdiesnana@gmail.com'),
 (4, 'kgoddard', 'ae3f68eb6c26aa33a282f0f3f077824e90dc7d0f7b537ffc17e4c489bebf40ec5c77fdc056f2c29f04bdeba7dc50055849126b02ccf384164af60402b154abc1', 'goddardken@outlook.com'),
-(5, 'tim', '919335f2439c6a07ef772cb0ef6a144b34c5eefd6bd4dfde5cc7527bf4b0fae6a93ef3bd9b232f34cfad5ac99542d8b205f5e5fdac2188dd6a7e18c8e8734fae', 'tim@nowhere.com');
+(5, 'tim', '8e2eba73083905e894f3119dc398a0fade61a9f88ad7f06e2a11409f29e9d1de85146b56a35b551e45e4ce7c827460e6eda207ee59e2cf04d2f32d5f15dbf7d8', 'tim@nowhere.com');
 
 --
 -- Indexes for dumped tables
